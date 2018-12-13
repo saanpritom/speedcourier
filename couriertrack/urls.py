44b3ab399f9111f0ac2d8e9b2f2a5add978ea771
saanpritom <pritom.saan@gmail.com>
2018-12-14 02:23:59 +0600

    HTML Template Installed and Configured

---
+++ couriertrack/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from couriertrack import views
+
+urlpatterns = [
+    path('', views.homepage, name='home-page'),
+    path('contact/', views.contactpage, name='contact-page'),
+    path('about/', views.aboutpage, name='about-page'),
+    path('tracking/', views.trackingpage, name='tracking-page'),
+]
--- couriertrack/views.py
+++ couriertrack/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    return render(request, 'home-page-view.html')
+
+
+def contactpage(request):
+    return render(request, 'contact-page-view.html')
+
+
+def aboutpage(request):
+    return render(request, 'about-us-page-view.html')
+
+
+def trackingpage(request):
+    return render(request, 'tracking-page-view.html')
