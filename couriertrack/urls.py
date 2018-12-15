e9c9d1b5ca6fd27f324a54170b229aa248037774
saanpritom <pritom.saan@gmail.com>
2018-12-15 17:10:25 +0600

    Partial Search Page Constructed

--- couriertrack/urls.py
+++ couriertrack/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('contact/', views.contactpage, name='contact-page'),
     path('about/', views.aboutpage, name='about-page'),
     path('tracking/', views.trackingpage, name='tracking-page'),
+    path('tracking/result', views.search, name='search_page'),
 ]
--- couriertrack/views.py
+++ couriertrack/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from couriertrack.models import StatusData, ParcelData, ParcelStatus
+from django.db.models import Q
+from itertools import chain
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+import operator
+from functools import reduce
 
 # Create your views here.
 def homepage(request):
@@ -15,3 +21,25 @@ def aboutpage(request):
 
 def trackingpage(request):
     return render(request, 'tracking-page-view.html')
+
+
+def search(request):
+    template_name = 'tracking-result-view.html'
+    dbdatas = request.GET.get('q').split(',')
+    mylist = [Q(parcel__parcel_number__contains=x) for x in dbdatas]
+    parcel_status = ParcelStatus.objects.filter(reduce(operator.or_,mylist))
+
+    context = {'search_items': parcel_status, 'query_text': dbdatas}
+    return render(request, template_name, context)
+
+    """try:
+        search_results = list(sorted(chain(parcel_status), key=lambda instance: instance.id))
+        total_results = len(search_results)
+        paginator = Paginator(search_results, 10)
+        page = request.GET.get('page')
+        s_results = paginator.get_page(page)
+        context = {'search_items': s_results, 'keyword': query, 'value': '1', 'total_results': total_results}
+        return render(request, template_name, context)
+    except IndexError as e:
+        context = {'value': '0'}
+        return render(request, template_name, context)"""
